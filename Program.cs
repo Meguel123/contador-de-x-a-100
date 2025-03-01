cbf03963bffc7738753a3167bf6b4aa86e638e19
Miguel P <miguel.perassoli@gmail.com>
2025-02-28 21:26:22 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{ 
+		int x;
+		
+	Console.WriteLine("fale um numero e contaremos até 100 com ele");
+		  x = Int32.Parse(Console.ReadLine());
+		for( int i = x; i < 101; i++){
+				
+	Console.WriteLine(i);}
+	}
+}
